7fd9e011788194ca3027ccde2eead0b37eb02994
gihudo <ihorgudzyk@gmail.com>
2023-02-12 23:43:17 +0100

    added selection sort to c++

---
+++ c++/Sorting/selectionsort.h
@@ -0,0 +1,16 @@
+#pragma once
+
+template<typename T>
+void selectionsort(T& arr, unsigned int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		int iMin = i;
+		for (int j = i + 1; j < size; j++)
+		{
+			if (arr[iMin] > arr[j])
+				iMin = j;
+		}
+		std::swap(arr[i], arr[iMin]);
+	}
+}
