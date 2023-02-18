68e1a79d0ec94ebe68c2707e06e0b3a8305284c3
Mima80 <mimi451276@gmail.com>
2023-02-18 20:48:30 +0200

    added c# folder and EvenOddInt class

---
+++ c#/Algoritms/EvenOddInt.cs
@@ -0,0 +1,264 @@
+﻿using System;
+
+public class EvenOddInt
+{
+    /// <summary>
+    /// returns true if num is even (0 < num <= 30)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+	public bool IsEven(int num)
+    {
+        if (num == 1)
+        {
+            return false;
+        }
+        else if (num == 2)
+        {
+            return true;
+        }
+        else if (num == 3)
+        {
+            return false;
+        }
+        else if (num == 4)
+        {
+            return true;
+        }
+        else if (num == 5)
+        {
+            return false;
+        }
+        else if (num == 6)
+        {
+            return true;
+        }
+        else if (num == 7)
+        {
+            return false;
+        }
+        else if (num == 8)
+        {
+            return true;
+        }
+        else if (num == 9)
+        {
+            return false;
+        }
+        else if (num == 10)
+        {
+            return true;
+        }
+        else if (num == 11)
+        {
+            return false;
+        }
+        else if (num == 12)
+        {
+            return true;
+        }
+        else if (num == 13)
+        {
+            return false;
+        }
+        else if (num == 14)
+        {
+            return true;
+        }
+        else if (num == 15)
+        {
+            return false;
+        }
+        else if (num == 16)
+        {
+            return true;
+        }
+        else if (num == 17)
+        {
+            return false;
+        }
+        else if (num == 18)
+        {
+            return true;
+        }
+        else if (num == 19)
+        {
+            return false;
+        }
+        else if (num == 20)
+        {
+            return true;
+        }
+        else if (num == 21)
+        {
+            return false;
+        }
+        else if (num == 22)
+        {
+            return true;
+        }
+        else if (num == 23)
+        {
+            return false;
+        }
+        else if (num == 24)
+        {
+            return true;
+        }
+        else if (num == 25)
+        {
+            return false;
+        }
+        else if (num == 26)
+        {
+            return true;
+        }
+        else if (num == 27)
+        {
+            return false;
+        }
+        else if (num == 28)
+        {
+            return true;
+        }
+        else if (num == 29)
+        {
+            return false;
+        }
+        else if (num == 30)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// returns true if num is odd (0 < num <= 30)
+    /// </summary>
+    /// <param name="num"></param>
+    /// <returns></returns>
+	public bool IsOdd(int num)
+    {
+        if (num == 1)
+        {
+            return true;
+        }
+        else if (num == 2)
+        {
+            return false;
+        }
+        else if (num == 3)
+        {
+            return true;
+        }
+        else if (num == 4)
+        {
+            return false;
+        }
+        else if (num == 5)
+        {
+            return true;
+        }
+        else if (num == 6)
+        {
+            return false;
+        }
+        else if (num == 7)
+        {
+            return true;
+        }
+        else if (num == 8)
+        {
+            return false;
+        }
+        else if (num == 9)
+        {
+            return true;
+        }
+        else if (num == 10)
+        {
+            return false;
+        }
+        else if (num == 11)
+        {
+            return true;
+        }
+        else if (num == 12)
+        {
+            return false;
+        }
+        else if (num == 13)
+        {
+            return true;
+        }
+        else if (num == 14)
+        {
+            return false;
+        }
+        else if (num == 15)
+        {
+            return true;
+        }
+        else if (num == 16)
+        {
+            return false;
+        }
+        else if (num == 17)
+        {
+            return true;
+        }
+        else if (num == 18)
+        {
+            return false;
+        }
+        else if (num == 19)
+        {
+            return true;
+        }
+        else if (num == 20)
+        {
+            return false;
+        }
+        else if (num == 21)
+        {
+            return true;
+        }
+        else if (num == 22)
+        {
+            return false;
+        }
+        else if (num == 23)
+        {
+            return true;
+        }
+        else if (num == 24)
+        {
+            return false;
+        }
+        else if (num == 25)
+        {
+            return true;
+        }
+        else if (num == 26)
+        {
+            return false;
+        }
+        else if (num == 27)
+        {
+            return true;
+        }
+        else if (num == 28)
+        {
+            return false;
+        }
+        else if (num == 29)
+        {
+            return true;
+        }
+        else if (num == 30)
+        {
+            return false;
+        }
+        return false;
+    }
+}
